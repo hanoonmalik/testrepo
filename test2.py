1ff593b4f8f07c92b6d855e4379a9f2399720a37
hanoonmalik <hannu_hell@hotmail.com>
2025-06-20 00:31:46 +0800

    created files

---
+++ test2.py
@@ -0,0 +1,7 @@
+a = [2,3,4,5,6]
+
+for i in a:
+	if a == 3:
+		print("OK")
+
+
---
+++ test3.py
@@ -0,0 +1,7 @@
+keys = {'hannu': 1, 'david': 3, 'vivian': 4}
+
+for k in keys:
+	if k['hannu'] == 3:
+		print("OK")
+
+
